c01135e4e4bd1de35cdb363442664f57ac980812
Joao Gosmani <joaogosmani@gmail.com>
2022-10-31 00:29:52 -0300

    First commit

---
+++ src/main/java/br/com/joaogosmani/achadoseperdidos/AchadosEPerdidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.achadoseperdidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchadosEPerdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchadosEPerdidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaogosmani/achadoseperdidos/AchadosEPerdidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.achadoseperdidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchadosEPerdidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
